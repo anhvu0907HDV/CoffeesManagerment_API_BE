309a9a9b69fe13bd26bd05e6b6b4d6da8a520a43
trantiendat2709 <tiendattran086@gmail.com>
2025-03-06 22:55:01 +0700

    commit API product

---
+++ Assignment_PRN231_API/Repository/IRepository/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using Assignment_PRN231_API.DTOs.Owner;
+using Assignment_PRN231_API.DTOs.Shop;
+
+namespace Assignment_PRN231_API.Repository.IRepository
+{
+    public interface IProductRepository
+    {
+        Task<List<ProductDto>> GetAllProducts();
+
+        Task<ProductDto> CreateProduct(ProductDto productDto);
+
+        Task<ProductDto> UpdateProduct(ProductDto productDto);
+    }
+}
